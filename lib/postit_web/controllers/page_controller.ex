bcdd13d560be81ab0a342e2832991978d7afddcf
Jesse Tomchak <jtomchak@gmail.com>
2019-07-09 21:37:11 -0700

    add current user to page controller

--- lib/postit_web/controllers/page_controller.ex
+++ lib/postit_web/controllers/page_controller.ex
@@ -5,6 +5,6 @@ defmodule PostitWeb.PageController do
 
   def index(conn, _params) do
   events = Events.list_future_events()
-    render(conn, "index.html", events: events) #guessing this binds the events for the template?
+    render(conn, "index.html", events: events, current_user: get_session(conn, :current_user)) #guessing this binds the events for the template?
   end
 end
