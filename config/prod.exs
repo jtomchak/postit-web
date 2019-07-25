4d21d135da809e3641fcc0496e87e38fa10e5ae2
Jesse Tomchak <jtomchak@gmail.com>
2019-07-24 21:27:15 -0700

    force ssl in prod

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :postit, PostitWeb.Endpoint,
   server: true,
   root: ".",
   version: Application.spec(:postit, :vsn),
-  rewrite_on: [:x_forwarded_proto]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 # Do not print debug messages in production
 config :logger, level: :info
 
