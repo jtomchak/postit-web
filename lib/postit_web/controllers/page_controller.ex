fe92f8fa3f5dbd779e34766ca62b05dc1ea38b43
Jesse Tomchak <jtomchak@gmail.com>
2019-08-07 21:19:37 -0700

    list all posts main page

--- lib/postit_web/controllers/page_controller.ex
+++ lib/postit_web/controllers/page_controller.ex
@@ -1,10 +1,20 @@
 defmodule PostitWeb.PageController do
   use PostitWeb, :controller
-  alias Postit.Events # what's an alias for?
+  require Logger
+  alias Postit.Posting
 
+  plug PostitWeb.Plugs.AuthenticateUser
+
+
+  # def index(conn, _params) do
+  # events = Events.list_future_events()
+  #   render(conn, "index.html", events: events, current_user: get_session(conn, :current_user)) #guessing this binds the events for the template?
+  # end
 
   def index(conn, _params) do
-  events = Events.list_future_events()
-    render(conn, "index.html", events: events, current_user: get_session(conn, :current_user)) #guessing this binds the events for the template?
+    posts = Posting.get_posts_by(conn.assigns.current_user.id)
+    Logger.info("posts: #{inspect posts}")
+    render(conn, "index.html", posts: posts, current_user: get_session(conn, :current_user))
   end
 end
+
