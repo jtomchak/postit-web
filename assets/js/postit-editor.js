aa4650d3823df5e1e0ca88b96493eaad1457d2b2
Jesse Tomchak <jtomchak@gmail.com>
2019-11-25 15:36:18 -0700

    capture submit, get view state to postcontent area

--- assets/js/postit-editor.js
+++ assets/js/postit-editor.js
@@ -1,20 +1,59 @@
 import { EditorState } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
 import { Schema, DOMParser } from "prosemirror-model"
-import { schema } from "prosemirror-schema-basic"
+import { schema as schemaBasic } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
 import { exampleSetup } from "prosemirror-example-setup"
+import { schema, defaultMarkdownParser, defaultMarkdownSerializer } from "prosemirror-markdown"
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks
+  nodes: addListNodes(schemaBasic.spec.nodes, "paragraph block*", "block"),
+  marks: schemaBasic.spec.marks
 })
 
-window.view = new EditorView(document.querySelector("#postit-editor"), {
-  state: EditorState.create({
-    schema: mySchema,
-    plugins: exampleSetup({ schema: mySchema })
-  })
+let postitForm = document.querySelector("#postit-content-form")
+let place = document.querySelector("#postit-md-wrapper")
+let postContent = document.querySelector("#post_content")
+console.log(postitForm)
+class MarkdownView {
+  constructor(target, content = "") {
+    this.textarea = target
+    this.textarea.value = content;
+  }
+
+  get content() { return this.textarea.value }
+  focus() { this.textarea.focus() }
+  destroy() { this.textarea.remove() }
+}
+
+class ProseMirrorView {
+  constructor(target, content = "") {
+    this.view = new EditorView(target, {
+      state: EditorState.create({
+        doc: defaultMarkdownParser.parse(content),
+        plugins: exampleSetup({ schema })
+      })
+    })
+  }
+
+  get content() {
+    return defaultMarkdownSerializer.serialize(this.view.state.doc)
+  }
+  focus() { this.view.focus() }
+  destroy() { this.view.destroy() }
+}
+
+
+let view = new ProseMirrorView(place)
+window.view = view
+
+// capture form onSubmit and serialize content then **submit** form
+postitForm.addEventListener('submit', function onSubmit(event) {
+  event.preventDefault();
+  postContent.value = view.content
+  postitForm.submit();
 })
+view.focus();
+
